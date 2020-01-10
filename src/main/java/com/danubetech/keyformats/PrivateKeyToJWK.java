f900622abbe6325df5bdb58c799beb811b6e6688
Markus Sabadello <markus@danubetech.com>
2020-01-10 13:12:29 +0100

    Return more specific subclasses.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/main/java/com/danubetech/keyformats/PrivateKeyToJWK.java
+++ src/main/java/com/danubetech/keyformats/PrivateKeyToJWK.java
@@ -7,15 +7,14 @@ import org.bitcoinj.core.ECKey;
 import org.bouncycastle.math.ec.ECPoint;
 
 import com.nimbusds.jose.jwk.Curve;
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.util.Base64URL;
 
 public class PrivateKeyToJWK {
 
-	public static JWK RSAPrivateKeyToJWK(RSAPrivateKey privateKey, RSAPublicKey publicKey, String kid, String use) {
+	public static com.nimbusds.jose.jwk.RSAKey RSAPrivateKeyToJWK(RSAPrivateKey privateKey, RSAPublicKey publicKey, String kid, String use) {
 
-		JWK jsonWebKey = new com.nimbusds.jose.jwk.RSAKey.Builder(publicKey)
+		com.nimbusds.jose.jwk.RSAKey jsonWebKey = new com.nimbusds.jose.jwk.RSAKey.Builder(publicKey)
 				.privateKey(privateKey)
 				.keyID(kid)
 				.keyUse(use == null ? null : new KeyUse(use))
@@ -24,7 +23,7 @@ public class PrivateKeyToJWK {
 		return jsonWebKey;
 	}
 
-	public static JWK P_256KPrivateKeyToJWK(ECKey privateKey, String kid, String use) {
+	public static com.nimbusds.jose.jwk.ECKey P_256KPrivateKeyToJWK(ECKey privateKey, String kid, String use) {
 
 		ECPoint publicKeyPoint = privateKey.getPubKeyPoint();
 		byte[] privateKeyBytes = privateKey.getPrivKeyBytes();
@@ -32,7 +31,7 @@ public class PrivateKeyToJWK {
 		Base64URL yParameter = Base64URL.encode(publicKeyPoint.getAffineYCoord().getEncoded());
 		Base64URL dParameter = Base64URL.encode(privateKeyBytes);
 
-		JWK jsonWebKey = new com.nimbusds.jose.jwk.ECKey.Builder(Curve.P_256K, xParameter, yParameter)
+		com.nimbusds.jose.jwk.ECKey jsonWebKey = new com.nimbusds.jose.jwk.ECKey.Builder(Curve.P_256K, xParameter, yParameter)
 				.d(dParameter)
 				.keyID(kid)
 				.keyUse(use == null ? null : new KeyUse(use))
@@ -41,19 +40,19 @@ public class PrivateKeyToJWK {
 		return jsonWebKey;
 	}
 
-	public static JWK P_256KPrivateKeyBytesToJWK(byte[] privateKeyBytes, String kid, String use) {
+	public static com.nimbusds.jose.jwk.ECKey P_256KPrivateKeyBytesToJWK(byte[] privateKeyBytes, String kid, String use) {
 
 		ECKey privateKey = ECKey.fromPrivate(privateKeyBytes);
 
 		return P_256KPrivateKeyToJWK(privateKey, kid, use);
 	}
 
-	public static JWK Ed25519PrivateKeyBytesToJWK(byte[] privateKeyBytes, byte[] publicKeyBytes, String kid, String use) {
+	public static com.nimbusds.jose.jwk.OctetKeyPair Ed25519PrivateKeyBytesToJWK(byte[] privateKeyBytes, byte[] publicKeyBytes, String kid, String use) {
 
 		Base64URL xParameter = Base64URL.encode(publicKeyBytes);
 		Base64URL dParameter = Base64URL.encode(privateKeyBytes);
 
-		JWK jsonWebKey = new com.nimbusds.jose.jwk.OctetKeyPair.Builder(Curve.Ed25519, xParameter)
+		com.nimbusds.jose.jwk.OctetKeyPair jsonWebKey = new com.nimbusds.jose.jwk.OctetKeyPair.Builder(Curve.Ed25519, xParameter)
 				.d(dParameter)
 				.keyID(kid)
 				.keyUse(use == null ? null : new KeyUse(use))
@@ -62,12 +61,12 @@ public class PrivateKeyToJWK {
 		return jsonWebKey;
 	}
 
-	public static JWK X25519PrivateKeyBytesToJWK(byte[] privateKeyBytes, byte[] publicKeyBytes, String kid, String use) {
+	public static com.nimbusds.jose.jwk.OctetKeyPair X25519PrivateKeyBytesToJWK(byte[] privateKeyBytes, byte[] publicKeyBytes, String kid, String use) {
 
 		Base64URL xParameter = Base64URL.encode(publicKeyBytes);
 		Base64URL dParameter = Base64URL.encode(privateKeyBytes);
 
-		JWK jsonWebKey = new com.nimbusds.jose.jwk.OctetKeyPair.Builder(Curve.X25519, xParameter)
+		com.nimbusds.jose.jwk.OctetKeyPair jsonWebKey = new com.nimbusds.jose.jwk.OctetKeyPair.Builder(Curve.X25519, xParameter)
 				.d(dParameter)
 				.keyID(kid)
 				.keyUse(use == null ? null : new KeyUse(use))
