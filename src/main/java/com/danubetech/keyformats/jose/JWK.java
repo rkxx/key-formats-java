2e02ed57e7b8f3c5dd29b857edb348226ad006c5
Markus Sabadello <markus@danubetech.com>
2021-08-04 10:12:46 +0200

    refactor: Better methods for (de-)serialization.

--- src/main/java/com/danubetech/keyformats/jose/JWK.java
+++ src/main/java/com/danubetech/keyformats/jose/JWK.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.codec.binary.Base64;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class JWK {
     private String kid;
@@ -21,19 +24,54 @@ public class JWK {
     public JWK() {
     }
 
+    /*
+     * Serialization
+     */
+
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    public static JWK fromJson(String json) throws IOException {
+        return objectMapper.readValue(json, JWK.class);
+    }
+
+    public static JWK fromJson(Reader reader) throws IOException {
+        return objectMapper.readValue(reader, JWK.class);
+    }
+
+    public static JWK fromMap(Map<String, Object> map) throws IOException {
+        return objectMapper.convertValue(map, JWK.class);
+    }
+
+    public Map<String, Object> toMap() {
+        return objectMapper.convertValue(this, LinkedHashMap.class);
+    }
+
+    public String toJson() {
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException("Cannot write JSON: " + ex.getMessage(), ex);
+        }
+    }
+
+    @Deprecated
     public static JWK parse(Map<String, Object> json) {
         return objectMapper.convertValue(json, JWK.class);
     }
 
+    @Deprecated
     public static JWK parse(String string) throws IOException {
         return objectMapper.readValue(new StringReader(string), JWK.class);
     }
 
+    @Deprecated
     public String toJSONString() throws JsonProcessingException {
         return objectMapper.writeValueAsString(this);
     }
+
+    /*
+     * Getters and setters
+     */
 
     public String getKid() {
         return kid;
@@ -107,5 +145,35 @@ public class JWK {
     public byte[] getDdecoded() {
         String d = this.getD();
         return d != null ? Base64.decodeBase64(d) : null;
+    }
+
+    /*
+     * Object methods
+     */
+
+    @Override
+    public String toString() {
+        return "JWK{" +
+                "kid='" + kid + '\'' +
+                ", use='" + use + '\'' +
+                ", kty='" + kty + '\'' +
+                ", crv='" + crv + '\'' +
+                ", x='" + x + '\'' +
+                ", y='" + y + '\'' +
+                ", d='" + d + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JWK jwk = (JWK) o;
+        return Objects.equals(kid, jwk.kid) && Objects.equals(use, jwk.use) && Objects.equals(kty, jwk.kty) && Objects.equals(crv, jwk.crv) && Objects.equals(x, jwk.x) && Objects.equals(y, jwk.y) && Objects.equals(d, jwk.d);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(kid, use, kty, crv, x, y, d);
     }
 }
