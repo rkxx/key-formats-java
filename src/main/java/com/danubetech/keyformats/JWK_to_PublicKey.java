30390acde9871f3abdf4714d34576ceac03e8bd8
Markus Sabadello <markus@danubetech.com>
2021-08-31 15:39:24 +0200

    fix: Fix public key byte encoding for secp256k1.

--- src/main/java/com/danubetech/keyformats/JWK_to_PublicKey.java
+++ src/main/java/com/danubetech/keyformats/JWK_to_PublicKey.java
@@ -7,11 +7,16 @@ import com.danubetech.keyformats.jose.KeyType;
 import com.danubetech.keyformats.jose.KeyTypeName;
 import com.danubetech.keyformats.keytypes.KeyTypeName_for_JWK;
 import org.bitcoinj.core.ECKey;
+import org.bouncycastle.math.ec.ECAlgorithms;
+import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
 
 import java.math.BigInteger;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 
@@ -45,7 +50,7 @@ public class JWK_to_PublicKey {
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 			RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(new BigInteger(jsonWebKey.getNdecoded()), new BigInteger(jsonWebKey.getEdecoded()));
 			return (RSAPublicKey) keyFactory.generatePrivate(rsaPublicKeySpec);
-		} catch (Exception ex) {
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException ex) {
 			throw new RuntimeException(ex.getMessage(), ex);
 		}
 	}
@@ -62,7 +67,7 @@ public class JWK_to_PublicKey {
 		if (! KeyType.EC.equals(jsonWebKey.getKty())) throw new IllegalArgumentException("Incorrect key type: " + jsonWebKey.getKty());
 		if (! Curve.secp256k1.equals(jsonWebKey.getCrv())) throw new IllegalArgumentException("Incorrect curve: " + jsonWebKey.getCrv());
 
-		return ECKey.fromPublicOnly(jsonWebKey.getXdecoded());
+		return ECKey.fromPublicOnly(JWK_to_secp256k1PublicKeyBytes(jsonWebKey));
 	}
 
 	public static byte[] JWK_to_secp256k1PublicKeyBytes(JWK jsonWebKey) {
@@ -70,7 +75,17 @@ public class JWK_to_PublicKey {
 		if (! KeyType.EC.equals(jsonWebKey.getKty())) throw new IllegalArgumentException("Incorrect key type: " + jsonWebKey.getKty());
 		if (! Curve.secp256k1.equals(jsonWebKey.getCrv())) throw new IllegalArgumentException("Incorrect curve: " + jsonWebKey.getCrv());
 
-		return jsonWebKey.getXdecoded();
+		byte[] xDecoded = jsonWebKey.getXdecoded();
+		if (xDecoded.length != 32) throw new IllegalArgumentException("Invalid 'x' value (not 32 bytes): " + jsonWebKey.getX() + ", length=" + jsonWebKey.getXdecoded().length);
+		byte[] yDecoded = jsonWebKey.getYdecoded();
+		if (yDecoded.length != 32) throw new IllegalArgumentException("Invalid 'y' value (not 32 bytes): " + jsonWebKey.getY() + ", length=" + jsonWebKey.getYdecoded().length);
+
+		byte[] publicKeyBytes = new byte[65];
+		publicKeyBytes[0] = 4;
+		System.arraycopy(xDecoded, 0, publicKeyBytes, 1, 32);
+		System.arraycopy(yDecoded, 0, publicKeyBytes, 33, 32);
+
+		return publicKeyBytes;
 	}
 
 	public static byte[] JWK_to_BLS12381_G1PublicKeyBytes(JWK jsonWebKey) {
